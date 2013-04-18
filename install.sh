cee997406f00043395236b2337c0088e8e250405
glejeune <gregoire.lejeune@gmail.com>
2013-04-18 10:22:34 +0200

    Install fonts for Powerline on Linux

--- install.sh
+++ install.sh
@@ -18,6 +18,17 @@ install_powerline_fonts() {
     git clone https://github.com/Lokaltog/powerline-fonts.git
     find powerline-fonts -type f -iname "*.[o|t]tf" -exec cp {} ~/Library/Fonts \;
     rm -rf powerline-fonts
+  elif [ "$PLATFORM" == "linux" ] ; then
+    cd $INSTALL_ROOT_PATH
+    wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
+    wget https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+    mkdir -p ~/.fonts
+    mv PowerlineSymbols.otf ~/.fonts
+    fc-cache -vf ~/.fonts 
+    mkdir -p ~/.fonts.conf.d/
+    mkdir -p ~/.config/fontconfig/conf.d/
+    cp 10-powerline-symbols.conf ~/.fonts.conf.d/
+    mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
   fi
 }
 
