2d706a370675170137c0064ff5286140bae7f4bb
glejeune <gregoire.lejeune@free.fr>
2013-07-09 18:31:30 +0200

    Deactivate powerline

---
+++ uninstall.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm ~/.zsh
+rm ~/.vimrc.bundle
+rm ~/.screenrc
+rm ~/.vimrc
+rm ~/.vim
+rm ~/.ctags
+rm ~/.muttrc
+rm ~/.muttrc.gmail
+rm ~/.zshrc
+rm ~/.tmux.conf
+rm -rf ~/.dotrc/_vim/bundle
+rm -f ~/.fonts/PowerlineSymbols.otf
+rm -f ~/.fonts.conf.d/10-powerline-symbols.conf
